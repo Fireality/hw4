10e3c723ac50a66912bcb743067ce1809f9ae891
root <root@LAPTOP-E12KB8S9.localdomain>
2023-03-20 22:45:52 -0700

    new code

--- sort.py
+++ sort.py
@@ -1,5 +1,5 @@
 def sort_dictionary(dictionary):
-	dictionary_order = sorted(dictionary.items(), key = lambda x: x[1][1], reverse = True)
-	dictionary_output = [(i, element[0]) for i, element in dictionary_order]
+	dictionary_order = sorted(dictionary.items(), key = lambda x: x[1][1])
+	dictionary_output = [(name, element[0]) for name, (phone_num, age) in dictionary_order]
 	return dictionary_output
 
