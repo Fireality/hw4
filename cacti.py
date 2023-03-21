4327cde552b1b1253c855436457e3582f4f572e9
root <root@LAPTOP-E12KB8S9.localdomain>
2023-03-21 00:00:16 -0700

    new code

--- cacti.py
+++ cacti.py
@@ -37,26 +37,26 @@ def cacti_number(a)
 	for i in range(rows):
 		for p in range (column):
 			if plant[i][p] == 0:
-			diagonal = False
-			if i > 0 and plant[i-1][p] == 1:
-				diagonal = True
-			if i < rows-1 and plant[i+1][p] == 1:
-				diagonal = True
-			if p > 0 and plant[i][p-1] == 1:
-				diagonal == True
-			if p < column -1 and plant[i][p+1] == 1:
-				diagonal = True
-			if i > 0 and p > 0 and plot[i-1][p-1] == 1:
-				diagonal = True
-			if i > 0 and p < column -1 and plant[i-1][p+1] == 1:
-				diagonal = True
-			if i < rows -1 and p > 0 and plant[i+1][p-1] == 1:
-				diagonal = True
-			if i < rows -1 and p <column -1 and plot[i+1][p+1] == 1:
-				diagonal = True
-			if not diagonal:
-				test_plant = [row[:] for row in plant]
-			test_plant[i][p] = 1
+				diagonal = False
+				if i > 0 and plant[i-1][p] == 1:
+					diagonal = True
+				if i < rows-1 and plant[i+1][p] == 1:
+					diagonal = True
+				if p > 0 and plant[i][p-1] == 1:
+					diagonal == True
+				if p < column -1 and plant[i][p+1] == 1:
+					diagonal = True
+				if i > 0 and p > 0 and plot[i-1][p-1] == 1:
+					diagonal = True
+				if i > 0 and p < column -1 and plant[i-1][p+1] == 1:
+					diagonal = True
+				if i < rows -1 and p > 0 and plant[i+1][p-1] == 1:
+					diagonal = True
+				if i < rows -1 and p <column -1 and plot[i+1][p+1] == 1:
+					diagonal = True
+				if not diagonal:
+					test_plant = [row[:] for row in plant]
+				test_plant[i][p] = 1
 			cacti_count = count_cacti(test_plant)
 			if cacti_count > max_cacti:
 				max_cacti = cacti_count
