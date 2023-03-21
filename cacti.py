cfc311e50e14e3e996135bd85395eb64c966edfc
root <root@LAPTOP-E12KB8S9.localdomain>
2023-03-20 21:28:54 -0700

    cacti code

---
+++ cacti.py
@@ -0,0 +1,27 @@
+def cacti_number(plant):
+	rows = len(plant)
+	column = len(plot)
+	count = 0
+
+	for i in range(rows):
+		for p in range (column):
+			if plant[i][p] == 0:
+				diagonal = True
+				if i > 0 and plant[i][p-1] == 1:
+					diagonal = False
+				if p > 0 and plant[i][p-1] == 1:
+					diagonal = False
+				if i < rows -1 and plant[i+1][p] == 1:
+					diagonal = False
+				if p < column -1 and plant[i][p+1] == 1:
+					diagonal = False
+				if i > 0 and p > 0 and plant[i-1][p-1] == 1:
+					diagonal = False
+				if i > 0 and p < column - 1 and plot [i-1][p+1] == 1:
+					diagonal = False
+				if i < rows -1 and p > 0 and plant[i+1][p-1] == 1:
+					diagonal = False
+				if i < rows -1 and p < column -1 and plant[i+1][p+1] == 1:
+					diagonal = False
+				if diagonal:
+					count = count + 1
